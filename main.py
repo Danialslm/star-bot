242e10ec6621447682ff0c9073760a137af6245b
Danial selmipoor <danial9713@gmail.com>
2022-02-19 00:58:41 +0330

    get users from database

--- main.py
+++ main.py
@@ -2,51 +2,63 @@
 # -*- coding: utf-8 -*-
 import logging
 
+from decouple import config
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 from telegram import ReplyKeyboardMarkup
-from telegram.ext import Updater, CommandHandler, Filters
+from telegram.ext import Updater, CommandHandler
 
-import settings
-from config import config_uc_handler, reset_checkout_list_handler, stop_ordering_handler, start_ordering_handler, \
-    reset_checkout_list_query_handler, send_notification_handler
-from ordering import ordering_handler, show_checkout_list_handler, paid_handler
+import models
+from models import Base
+
+# environments
+CONFIG_ADMIN = config('CONFIG_ADMIN', cast=int)
+DEBUG = config('DEBUG', default=False)
 
 # logging
-if settings.DEBUG:
-    logging.basicConfig(
-        level=logging.DEBUG,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    )
-else:
-    logging.basicConfig(
-        level=logging.ERROR,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        filename='error.log'
-    )
+logging_config = {
+    'level': logging.DEBUG,
+    'format': '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+}
+if not DEBUG:
+    logging_config.update({
+        'level': logging.ERROR,
+        'filename': 'log/error.log',
+    })
+
+logging.basicConfig(**logging_config)
 logger = logging.getLogger(__name__)
 
-ORDER_ADMINS = settings.ORDER_ADMINS_GAP_1 + settings.ORDER_ADMINS_GAP_2
+# create a db engine and session maker and create all tables
+engine = create_engine('sqlite:///star_bot.db', echo=DEBUG)  # echo queries in debug mode
+Base.metadata.create_all(engine)
+Session = sessionmaker(bind=engine)
 
-ADMINS = ORDER_ADMINS + [settings.NOTIFY_SENDER, settings.CONFIG_ADMIN]
+session = Session()
 
 
 def start(update, context):
-    if not settings.DEBUG:
-        keyboard = []
-        if update.message.chat_id in ORDER_ADMINS:
-            keyboard = [
-                ['ثبت سفارش جدید'],
-                ['نمایش لیست تسویه حساب'],
-            ]
-        elif update.message.chat_id == settings.CONFIG_ADMIN:
+    if not DEBUG:
+        chat_id = update.message.chat_id
+
+        if chat_id == CONFIG_ADMIN:
             keyboard = [
                 ['بروزرسانی لیست یوسی ها'],
                 ['ریست لیست تسویه حساب'],
                 ['قفل سفارش'],
                 ['بازکردن سفارش'],
             ]
-        elif update.message.chat_id == settings.NOTIFY_SENDER:
+        else:
+            admin = session.query(models.Admin.user_chat_id).filter(
+                models.Admin.user_chat_id == chat_id,
+            ).all()
+            # if the user does not admin, do nothing
+            if not admin:
+                return
+
             keyboard = [
-                ['اطلاعیه'],
+                ['ثبت سفارش جدید'],
+                ['نمایش لیست تسویه حساب'],
             ]
     else:
         keyboard = [
@@ -60,7 +72,6 @@ def start(update, context):
         ]
 
     reply_markup = ReplyKeyboardMarkup(keyboard, resize_keyboard=True)
-
     update.message.reply_text(
         'لطفا یکی از گزینه های زیر را انتخاب کنید.',
         reply_markup=reply_markup,
@@ -76,25 +87,22 @@ def error_handler(update, context):
 
 
 def main():
-    updater = Updater(settings.TOKEN, use_context=True)
+    token = config('TOKEN')
+    updater = Updater(token, use_context=True)
     dp = updater.dispatcher
 
-    dp.add_handler(CommandHandler(
-        'start',
-        start,
-        filters=Filters.chat(ADMINS))
-    )
+    dp.add_handler(CommandHandler('start', start))
     dp.add_handler(CommandHandler('online', is_online))
-    dp.add_handler(ordering_handler)
-    dp.add_handler(show_checkout_list_handler)
-    dp.add_handler(paid_handler)
-    dp.add_handler(config_uc_handler)
-    dp.add_handler(reset_checkout_list_handler)
-    dp.add_handler(reset_checkout_list_query_handler)
-    dp.add_handler(stop_ordering_handler)
-    dp.add_handler(start_ordering_handler)
-    dp.add_handler(send_notification_handler)
-    dp.add_error_handler(error_handler)
+    # dp.add_handler(ordering_handler)
+    # dp.add_handler(show_checkout_list_handler)
+    # dp.add_handler(paid_handler)
+    # dp.add_handler(config_uc_handler)
+    # dp.add_handler(reset_checkout_list_handler)
+    # dp.add_handler(reset_checkout_list_query_handler)
+    # dp.add_handler(stop_ordering_handler)
+    # dp.add_handler(start_ordering_handler)
+    # dp.add_handler(send_notification_handler)
+    # dp.add_error_handler(error_handler)
 
     updater.start_polling()
     updater.idle()

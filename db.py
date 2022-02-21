f883d7d3d4404e365bd671323ea14a232ff5565d
Danial selmipoor <danial9713@gmail.com>
2022-02-21 16:05:22 +0330

    no checking same thread

--- db.py
+++ db.py
@@ -4,7 +4,11 @@ from sqlalchemy.orm import sessionmaker
 from env import DEBUG
 
 # create a db engine and session maker
-engine = create_engine('sqlite:///star_bot.db', echo=bool(DEBUG))  # echo queries in debug mode
+engine = create_engine(
+    'sqlite:///star_bot.db',
+    echo=bool(DEBUG),
+    connect_args={"check_same_thread": False}
+)  # echo queries in debug mode
 Session = sessionmaker(bind=engine)
 
 

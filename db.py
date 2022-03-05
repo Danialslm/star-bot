67141206d55ce71df7dbfa38ad63f2519554f7cd
Danial selmipoor <danial9713@gmail.com>
2022-03-06 01:08:10 +0330

    modify db path

--- db.py
+++ db.py
@@ -1,11 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-
+from pathlib import Path
 from env import DEBUG
 
 # create a db engine and session maker
 engine = create_engine(
-    'sqlite:///star_bot.db',
+    f'sqlite:///{Path(__file__).parent / "star_bot.db"}',
     echo=bool(DEBUG),  # echo queries in debug mode
     connect_args={"check_same_thread": False},
 )
